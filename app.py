d31a2dada06d766ceb7eae0d35ad09b10fb84e62
KaatCAIBXL <kaat.dbolle@gmail.com>
2025-10-19 20:03:26 +0200

    Create app.py
    This is for inserting the app as a zoom extension.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(traduction CAI)
+
+@app.route("/zoom-bot-endpoint", methods=["POST"])
+def zoom_bot():
+    data = request.json
+    print("Zoom stuurde:", data)
+
+    return {
+        "messages": [
+            {
+                "text": "👋 Hallo! Je bot werkt en heeft je bericht ontvangen."
+            }
+        ]
+    }, 200
